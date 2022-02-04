c075e334d5e14ec0cd03f814c70270997f8f4426
Mark <mrk.c.fischer@gmail.com>
2022-02-04 19:21:20 +0000

    More Layout Tweaks

--- src/TallyView/SummaryTable.tsx
+++ src/TallyView/SummaryTable.tsx
@@ -1,5 +1,3 @@
-import * as React from 'react';
-
 import {
   Box,
   Table,
@@ -8,30 +6,37 @@ import {
   TableRow,
   Paper,
   TableHead,
-  tableCellClasses
-} from '@mui/material';
+  tableCellClasses,
+} from "@mui/material";
 
-import { tallyRound } from '../util';
+import { mergeDays, sum, tallyRound } from "../util";
+import {
+  endOfDay,
+  isThisYear,
+  sub,
+  isWithinInterval,
+  isToday,
+  isThisWeek,
+  isThisMonth,
+  getDayOfYear,
+} from "date-fns";
+import { Entry } from "../StorageService/store";
 
-export { SummaryTable }
+export { SummaryTable };
 
-function SummaryTable(params: {
-  dispList: {
-    label: string,
-    avg: number,
-    total: number
-  }[];
-}) {
+function SummaryTable({ entries }: { entries: Entry[] }) {
+  const dispList = summary(entries);
+
   return (
-    <Box sx={{ width: '100%' }}>
+    <Box sx={{ width: "100%" }}>
       <Paper elevation={1} sx={{ mx: 1 }}>
         <Table
           size="small"
           aria-label="a dense table"
           sx={{
             [`& .${tableCellClasses.root}`]: {
-              borderBottom: "none"
-            }
+              borderBottom: "none",
+            },
           }}
         >
           <TableHead>
@@ -42,22 +47,113 @@ function SummaryTable(params: {
             </TableRow>
           </TableHead>
           <TableBody>
-            {params.dispList.map(v =>
-              <TableRow key={v.label} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+            {dispList.map((v) => (
+              <TableRow
+                key={v.label}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
                 <TableCell component="th" scope="row">
                   <em>{v.label}</em>
                 </TableCell>
                 <TableCell align="right">
                   {v.avg !== v.total ? tallyRound(v.avg) : ""}
                 </TableCell>
-                <TableCell align="right">
-                  {tallyRound(v.total)}
-                </TableCell>
+                <TableCell align="right">{tallyRound(v.total)}</TableCell>
               </TableRow>
-            )}
+            ))}
           </TableBody>
         </Table>
       </Paper>
     </Box>
-  )
+  );
+}
+
+function summary(entries: Entry[]): {
+  label: string;
+  avg: number;
+  total: number;
+}[] {
+  
+  let dispList: {
+    label: string;
+    avg: number;
+    total: number;
+  }[] = [];
+
+  const mergedEntries = mergeDays(entries);
+
+  const currentDate = new Date();
+  const endOfToday = endOfDay(currentDate);
+
+  const thisYear = mergedEntries.filter((v) => isThisYear(v.date));
+
+  const interval30Days = {
+    start: sub(endOfToday, { days: 30 }),
+    end: endOfToday,
+  };
+  const last30Days = mergedEntries.filter((v) =>
+    isWithinInterval(v.date, interval30Days)
+  );
+  const interval7Days = {
+    start: sub(endOfToday, { days: 7 }),
+    end: endOfToday,
+  };
+  const last7Days = last30Days.filter((v) =>
+    isWithinInterval(v.date, interval7Days)
+  );
+  const today = last7Days.filter((v) => isToday(v.date));
+
+  const todayTotal = today[0]?.count || 0;
+  dispList.push({
+    label: "Today",
+    avg: todayTotal,
+    total: todayTotal,
+  });
+
+  const totalLast7Days = sum(last7Days.map((v) => v.count));
+  dispList.push({
+    label: "7 Days",
+    avg: totalLast7Days / 7,
+    total: totalLast7Days,
+  });
+
+  const totalLast30Days = sum(last30Days.map((v) => v.count));
+  dispList.push({
+    label: "30 Days",
+    avg: totalLast30Days / 30,
+    total: totalLast30Days,
+  });
+
+  const _dayOfTheWeek = currentDate.getDay();
+  const dayOfTheWeek = _dayOfTheWeek === 0 ? 7 : _dayOfTheWeek;
+  const totalThisWeek = sum(
+    last7Days
+      .filter((v) => isThisWeek(v.date, { weekStartsOn: 1 }))
+      .map((v) => v.count)
+  );
+  dispList.push({
+    label: "This Week",
+    avg: totalThisWeek / dayOfTheWeek,
+    total: totalThisWeek,
+  });
+
+  const dayOfTheMonth = currentDate.getDate();
+  const totalThisMonth = sum(
+    last30Days.filter((v) => isThisMonth(v.date)).map((v) => v.count)
+  );
+  dispList.push({
+    label: "This Month",
+    avg: totalThisMonth / dayOfTheMonth,
+    total: totalThisMonth,
+  });
+
+  const dayOfTheYear = getDayOfYear(currentDate);
+  const totalThisYear = sum(thisYear.map((v) => v.count));
+  dispList.push({
+    label: "This Year",
+    avg: totalThisYear / dayOfTheYear,
+    total: totalThisYear,
+  });
+
+  return dispList;
 }
--- src/TallyView/TallyView.tsx
+++ src/TallyView/TallyView.tsx
@@ -1,21 +1,10 @@
 import { Dispatch } from "react";
 import { Box, Typography } from "@mui/material";
-import {
-  endOfDay,
-  getDayOfYear,
-  isThisMonth,
-  isThisWeek,
-  isThisYear,
-  isToday,
-  sub,
-  isWithinInterval,
-} from "date-fns";
 
 import { compareEntryTimeDesc, Entry } from "../StorageService/store";
 import { EntriesTable } from "./EntriesTable";
 import { SummaryTable } from "./SummaryTable";
 import { TallyWriter } from "./TallyWriter";
-import { mergeDays, sum } from "../util";
 import { TagStateEntries } from "../App";
 import { StoreAction } from "../StorageService/store-reducer";
 
@@ -31,13 +20,12 @@ function TallyView({
   storeDispatch: Dispatch<StoreAction>;
 }) {
   const entriesLoading = entries === "Loading";
-  const dispList = entriesLoading ? [] : summary(entries);
 
   const tallyButtons = entriesLoading ? [] : decideWritterButtons(entries);
 
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", gap: 4 }}>
-      <Typography variant="h3" sx={{ marginTop: 4 }}>
+    <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+      <Typography variant="h3" sx={{ marginTop: 3 }}>
         <strong style={{ fontWeight: 550 }}>{tag}</strong>
       </Typography>
       {entriesLoading ? (
@@ -55,11 +43,11 @@ function TallyView({
         sx={{
           display: !entriesLoading && entries.length > 0 ? "flex" : "none",
           flexDirection: "column",
-          marginBottom: 4,
-          gap: 4,
+          marginBottom: 2,
+          gap: 2,
         }}
       >
-        <SummaryTable dispList={dispList} />
+        <SummaryTable entries={entriesLoading ? [] : entries} />
         <EntriesTable
           tag={tag}
           entries={entriesLoading ? [] : entries}
@@ -68,95 +56,6 @@ function TallyView({
       </Box>
     </Box>
   );
-}
-
-function summary(entries: Entry[]): {
-  label: string;
-  avg: number;
-  total: number;
-}[] {
-  let dispList: {
-    label: string;
-    avg: number;
-    total: number;
-  }[] = [];
-
-  const mergedEntries = mergeDays(entries);
-
-  const currentDate = new Date();
-  const endOfToday = endOfDay(currentDate);
-
-  const thisYear = mergedEntries.filter((v) => isThisYear(v.date));
-
-  const interval30Days = {
-    start: sub(endOfToday, { days: 30 }),
-    end: endOfToday,
-  };
-  const last30Days = mergedEntries.filter((v) =>
-    isWithinInterval(v.date, interval30Days)
-  );
-  const interval7Days = {
-    start: sub(endOfToday, { days: 7 }),
-    end: endOfToday,
-  };
-  const last7Days = last30Days.filter((v) =>
-    isWithinInterval(v.date, interval7Days)
-  );
-  const today = last7Days.filter((v) => isToday(v.date));
-
-  const todayTotal = today[0]?.count || 0;
-  dispList.push({
-    label: "Today",
-    avg: todayTotal,
-    total: todayTotal,
-  });
-
-  const totalLast7Days = sum(last7Days.map((v) => v.count));
-  dispList.push({
-    label: "7 Days",
-    avg: totalLast7Days / 7,
-    total: totalLast7Days,
-  });
-
-  const totalLast30Days = sum(last30Days.map((v) => v.count));
-  dispList.push({
-    label: "30 Days",
-    avg: totalLast30Days / 30,
-    total: totalLast30Days,
-  });
-
-  const _dayOfTheWeek = currentDate.getDay();
-  const dayOfTheWeek = _dayOfTheWeek === 0 ? 7 : _dayOfTheWeek;
-  const totalThisWeek = sum(
-    last7Days
-      .filter((v) => isThisWeek(v.date, { weekStartsOn: 1 }))
-      .map((v) => v.count)
-  );
-  dispList.push({
-    label: "This Week",
-    avg: totalThisWeek / dayOfTheWeek,
-    total: totalThisWeek,
-  });
-
-  const dayOfTheMonth = currentDate.getDate();
-  const totalThisMonth = sum(
-    last30Days.filter((v) => isThisMonth(v.date)).map((v) => v.count)
-  );
-  dispList.push({
-    label: "This Month",
-    avg: totalThisMonth / dayOfTheMonth,
-    total: totalThisMonth,
-  });
-
-  const dayOfTheYear = getDayOfYear(currentDate);
-  const totalThisYear = sum(thisYear.map((v) => v.count));
-  dispList.push({
-    label: "This Year",
-    avg: totalThisYear / dayOfTheYear,
-    total: totalThisYear,
-  });
-
-  return dispList;
 }
 
 function decideWritterButtons(entries: Entry[]): number[] {
